3540bbfd520418bdd4ac513f4c121abe84c5a174
ntysdd <ntysdd@gmail.com>
2019-07-13 22:37:57 +0800

    open source file as FileInputStream

--- src/main/java/ntysdd/c/interpreter/App.java
+++ src/main/java/ntysdd/c/interpreter/App.java
@@ -1,6 +1,14 @@
 package ntysdd.c.interpreter;
 
-public class App {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import lombok.Cleanup;
+
+public final class App {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        @Cleanup
+        FileInputStream fis = new FileInputStream(fileName);
     }
 }
---
+++ src/main/java/ntysdd/c/interpreter/Token.java
@@ -0,0 +1,8 @@
+package ntysdd.c.interpreter;
+
+import lombok.Value;
+
+@Value
+public final class Token {
+    private final String string;
+}
